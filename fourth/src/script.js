a94d5d137aa23dc12704e84efac10f70010fa84d
Akash Shaw <shawakash2003@gmail.com>
2023-05-01 19:35:05 +0530

    Ways to get a increment value as well as playing

--- fourth/src/script.js
+++ fourth/src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 
+const pi = Math.PI
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -9,7 +10,7 @@ const scene = new THREE.Scene()
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: 'cyan' })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -31,12 +32,37 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 
+//===>>>> let initialTime = Date.now();
+//===>>>> Date.now has a value of time in milli-seconds since the starting of year
+//===>>>> Another way is to set a CLOCK at the starting of your website
+
+const clock = new THREE.Clock();
+
+
 // Animation
 const tick = () => {
-    mesh.rotation.y += 0.051;
-    mesh.rotation.x += 0.051;
-    mesh.rotation.z += 0.051;
+
+    // let currentTime = Date.now();
+    // let delta = currentTime - initialTime;
+    // initialTime = currentTime;  // update the time
+    let elapsedTime = clock.getElapsedTime();
+
+    // transform the group once
+    mesh.rotation.x = pi * elapsedTime;          // radians you want to rotate per second * elapsedTime
+    mesh.rotation.y = pi * elapsedTime;
+    mesh.rotation.z = pi * elapsedTime;
+
+    // mesh.position.x = Math.sin(elapsedTime*5)
+    // mesh.position.y = 2*Math.cos(elapsedTime*5)
+    // camera.position.x = Math.sin(elapsedTime*5)
+    // camera.position.y = 2*Math.cos(elapsedTime*5)
+    // camera.lookAt(mesh.position)
+
+    // Render The Scene once
     renderer.render(scene, camera)
+
+    // Call It Recurivly
+    // Rate of animation depends on FPS rating of cpu   ---> use time dfference as time is the only thing which is constant for all, or it isn't ?
     window.requestAnimationFrame(tick);
 }
 
