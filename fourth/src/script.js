51ecd14dde0275a536601fe1aedb2dbcb7f52ff9
Akash Shaw <shawakash2003@gmail.com>
2023-05-01 18:53:50 +0530

    First Animation On Three Js

---
+++ fourth/src/script.js
@@ -0,0 +1,43 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+
+// Animation
+const tick = () => {
+    mesh.rotation.y += 0.051;
+    mesh.rotation.x += 0.051;
+    mesh.rotation.z += 0.051;
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick);
+}
+
+tick()
