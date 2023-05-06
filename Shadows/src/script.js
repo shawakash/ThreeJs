0bb8f34f1c0898f4f3e95aeff82d7c191e384336
Akash Shaw <shawakash2003@gmail.com>
2023-05-06 11:54:10 +0530

    Adding Shadows On PointLight

--- Shadows/src/script.js
+++ Shadows/src/script.js
@@ -32,12 +32,12 @@ const scene = new THREE.Scene()
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3)
 directionalLight.position.set(2, 2, - 1)
 gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
@@ -75,7 +75,7 @@ directionalCameraHelper.visible = false
  * Spot Light
  */
 
-const spotLight = new THREE.SpotLight(0xffffff, 0.5, 10, Math.PI * 0.3);
+const spotLight = new THREE.SpotLight(0xffffff, 0.3, 10, Math.PI * 0.3);
 
 spotLight.castShadow = true;
 
@@ -98,7 +98,27 @@ scene.add(spotLight.target)
 const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
 scene.add(spotLightCameraHelper);
 
-// spotLightCameraHelper.visible = false;
+spotLightCameraHelper.visible = false;
+
+
+const pointLight = new THREE.PointLight(0xffffff, 0.3);
+
+pointLight.castShadow = true;
+pointLight.position.set(-1, 1, 0);
+
+pointLight.shadow.mapSize.width = 1024;
+pointLight.shadow.mapSize.height = 1024;
+pointLight.shadow.camera.near = 0.1;
+pointLight.shadow.camera.far = 3;
+
+
+scene.add(pointLight);
+// scene.add(pointLight.target);
+
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera);
+scene.add(pointLightCameraHelper);
+
+pointLightCameraHelper.visible = false;
 
 
 
