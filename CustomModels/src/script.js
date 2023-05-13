296ae56099d40e26f6c2a2edf9c47b0b7fa2de36
Akash Shaw <shawakash2003@gmail.com>
2023-05-13 13:31:21 +0530

    Exporting Models

--- CustomModels/src/script.js
+++ CustomModels/src/script.js
@@ -29,7 +29,7 @@ gltfLoader.setDRACOLoader(dracoLoader)
 let mixer = null
 
 gltfLoader.load(
-    '/models/hamburger.glb',
+    'models/hamburger.glb',
     (gltf) =>
     {
         scene.add(gltf.scene)
