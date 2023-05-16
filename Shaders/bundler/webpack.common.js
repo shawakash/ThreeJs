b9ecd3b0d95ec21cc5642a137e6df4011158b01a
Akash Shaw <shawakash2003@gmail.com>
2023-05-16 20:03:40 +0530

    Loading Shaders Data

--- Shaders/bundler/webpack.common.js
+++ Shaders/bundler/webpack.common.js
@@ -76,6 +76,22 @@ module.exports = {
                 {
                     filename: 'assets/fonts/[hash][ext]'
                 }
+            },
+
+            // Shaders
+            // {
+            //     test: /\.(glsl|vs|fs|vert|frag)$/,
+            //     exclude: /node_modules/,
+            //     use: [
+            //         'raw-loader'
+            //     ]
+            // }
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/,
+                type: 'asset/source',
+                generator: {
+                    filename: 'assets/images/[hash][ext]'
+                }
             }
         ]
     }
