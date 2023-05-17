46eb436a1d30c7133a380f3d10401659e84df064
Akash Shaw <shawakash2003@gmail.com>
2023-05-17 14:59:56 +0530

    Pattern 4

--- SPattern/src/shaders/test/fragment.glsl
+++ SPattern/src/shaders/test/fragment.glsl
@@ -10,5 +10,11 @@ void main()
     float strengthX = vUv.x;
     float strengthY = vUv.y;
 
-    gl_FragColor = vec4(vec3(strengthX), 1.0);
+    // vUv or uv goes from 0,0 from left bottom corner to 1,1 to right top corner
+    // Pattern strenght varies according to uv strenght, with uv.x the pattern strenght varies in x according to the coordinates
+    // when applied to certain color say r then intensity of r varies from 0 to 1 in x-direction
+    // Similary case with uUv.y;
+
+    //Pattern 4
+    // gl_FragColor = vec4(vec3(strengthY), 1.0);
 }
