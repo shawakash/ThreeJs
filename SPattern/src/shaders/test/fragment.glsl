f07bd38960b025dad620b15296dff18dbfa188f8
Akash Shaw <shawakash2003@gmail.com>
2023-05-17 14:24:32 +0530

    Adding First Pattern

--- SPattern/src/shaders/test/fragment.glsl
+++ SPattern/src/shaders/test/fragment.glsl
@@ -1,4 +1,9 @@
+
+// Varying
+varying vec2 vUv;
+
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    // vec4 textureColor = texture2D();
+    gl_FragColor = vec4(vUv, 1.0, 1.0);
 }
--- SPattern/src/shaders/test/vertex.glsl
+++ SPattern/src/shaders/test/vertex.glsl
@@ -1,4 +1,13 @@
+
+
+// Varying
+varying vec2 vUv;
+// vUv or uv goes from 0,0 from left bottom corner to 1,1 to right top corner
+
 void main()
 {
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+    // Varying
+    vUv = uv;
 }
