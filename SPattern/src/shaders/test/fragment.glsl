0747ec5827b62fdb88d80f983ea3bc807ed5c9c7
Akash Shaw <shawakash2003@gmail.com>
2023-05-18 17:51:56 +0530

    Pattern 11

--- SPattern/src/shaders/test/fragment.glsl
+++ SPattern/src/shaders/test/fragment.glsl
@@ -2,8 +2,7 @@
 // Varying
 varying vec2 vUv;
 
-void main()
-{
+void main() {
     // vec4 textureColor = texture2D();
 
     // Strength
@@ -23,10 +22,10 @@ void main()
 
     // Pattern 6
     // gl_FragColor = vec4(vec3(strengthY), 1.0);
-    
+
     // Pattern 7
     // gl_FragColor = vec4(vec3(mod(strengthY * 10.0, 1.0)), 1.0);
-    
+
     // Pattern 8
 //     float strength = mod(vUv.y * 10.0, 1.0);
 
@@ -42,20 +41,40 @@ void main()
 //     // } else {
 //     //     strength = 0.0;
 //     // }
-    
+
 //     gl_FragColor = vec4(vec3(strength), 1.0);
-    
+
     // Pattern 9
     // float strength = mod(vUv.y * 10.0, 1.0);
 
     // strength = step(0.8, strength);     // strength returns 0 if the value inside less than edge or else 1'
-    
-    // gl_FragColor = vec4(vec3(strength), 1.0);
-    
-    // Pattern 10
-    float strength = mod(vUv.x * 10.0, 1.0);
 
-    strength = step(0.8, strength);     // strength returns 0 if the value inside less than edge or else 1'
-    
+    // gl_FragColor = vec4(vec3(strength), 1.0);
+
+    // Pattern 10
+    // float strength = mod(vUv.x * 10.0, 1.0);
+
+    // strength = step(0.8, strength);     // strength returns 0 if the value inside less than edge or else 1'
+
+    // gl_FragColor = vec4(vec3(strength), 1.0);
+
+    // Pattern 11
+    // float subStrength = vUv.x;
+    float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+    // strength = step(0.8, strength);
+
+    // // if(strength == 1.0) {
+    // //     subStrength = vUv.x;
+    // // } else {
+    // //     subStrength = vUv.y;
+    // // }
+
+    // subStrength = strength == 1.0 ? vUv.x : vUv.y;
+
+    // strength = mod(subStrength * 10.0, 1.0);
+    // strength = step(0.8, strength);
+
+    strength += step(0.8, mod(vUv.y * 10.0, 1.0));
+
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
