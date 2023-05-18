16247d2ccf34a7f226e104101d960a7ac4814313
Akash Shaw <shawakash2003@gmail.com>
2023-05-18 18:17:24 +0530

    Pattern 14, 15

--- SPattern/src/shaders/test/fragment.glsl
+++ SPattern/src/shaders/test/fragment.glsl
@@ -94,9 +94,18 @@ void main() {
     
     // Pattern 14
 
-    float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
 
-    float barY = step(0.4, mod(vUv.y * 10.0, 1.0)) * step(0.8, mod(vUv.x * 10.0, 1.0));
+    // float barY = step(0.4, mod(vUv.y * 10.0, 1.0)) * step(0.8, mod(vUv.x * 10.0, 1.0));
+
+    // float strength = barX + barY;
+    
+    
+    // Pattern 15
+
+    float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
+
+    float barY = step(0.4, mod(vUv.y * 10.0 , 1.0)) * step(0.8, mod(vUv.x * 10.0  + 0.2, 1.0));
 
     float strength = barX + barY;
 
