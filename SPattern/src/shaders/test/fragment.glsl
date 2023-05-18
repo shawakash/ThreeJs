ee4e0001cf1166c86c47db65477100f342ff215d
Akash Shaw <shawakash2003@gmail.com>
2023-05-18 18:57:14 +0530

    Pattern 23, 24

--- SPattern/src/shaders/test/fragment.glsl
+++ SPattern/src/shaders/test/fragment.glsl
@@ -2,6 +2,10 @@
 // Varying
 varying vec2 vUv;
 
+float random(vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
 void main() {
     // vec4 textureColor = texture2D();
 
@@ -129,7 +133,17 @@ void main() {
     // float strength = floor(vUv.x * 10.0) / 10.0;
     
     // Pattern 22
-    float strength = floor(vUv.x * 10.0) / 10.0 * floor(vUv.y * 10.0) / 10.0;
+    // float strength = floor(vUv.x * 10.0) / 10.0 * floor(vUv.y * 10.0) / 10.0;
+    
+    // Pattern 23
+    // float strength = random(vUv);
+    
+    // Pattern 24
+    vec2 gridUv = vec2(
+        floor(vUv.x * 10.0) / 10.0,
+        floor(vUv.y * 10.0) / 10.0
+    );
+    float strength = random(gridUv);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
