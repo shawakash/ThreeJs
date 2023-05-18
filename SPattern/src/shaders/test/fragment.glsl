4f2d2e652dd7a241c4afaf1ec5f2205c52f7e8b0
Akash Shaw <shawakash2003@gmail.com>
2023-05-18 19:17:01 +0530

    Pattern 28, 29

--- SPattern/src/shaders/test/fragment.glsl
+++ SPattern/src/shaders/test/fragment.glsl
@@ -156,7 +156,14 @@ void main() {
     // float strength = sqrt(vUv.y * vUv.y + vUv.x * vUv.x);               // || || length(vUv)
     
     // Pattern 27
-    float strength = length(vUv - 0.5);
+    // float strength = length(vUv - 0.5);
+    // float strength = distance(vUv, vec2(0.5));
+    
+    // Pattern 28
+    // float strength = 1.0 - distance(vUv, vec2(0.5));
+    
+    // Pattern 29
+    float strength = 0.01 / distance(vUv, vec2(0.5));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
