b2b9063c6437a4e9c691cf4cd5a5f936998ad616
Akash Shaw <shawakash2003@gmail.com>
2023-05-20 14:11:52 +0530

    Diifused Circle

--- Galaxy2/src/shaders/galaxy/fragment.glsl
+++ Galaxy2/src/shaders/galaxy/fragment.glsl
@@ -1,7 +1,13 @@
 
 
 void main() {
-    float strength = step(0.5, 1.0 - distance(gl_PointCoord, vec2(0.5)));
+
+    // Disc Pattern
+    // float strength = step(0.5, 1.0 - distance(gl_PointCoord, vec2(0.5)));
+
+    // Diffuse Point Pattern
+    // Multiply by 2 as the gradient gets upto one and we were cutting only upto 0.5;
+    float strength =  1.0 - 2.0 * distance(gl_PointCoord, vec2(0.5));
 
     gl_FragColor = vec4(vec2(strength * gl_PointCoord), strength, 1.0);
 }
