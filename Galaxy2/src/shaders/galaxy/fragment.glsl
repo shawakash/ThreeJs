504e2ee5f17acb1a72a88ab147ec64168563cb2d
Akash Shaw <shawakash2003@gmail.com>
2023-05-20 14:14:19 +0530

    Light Point Diifused Circle

--- Galaxy2/src/shaders/galaxy/fragment.glsl
+++ Galaxy2/src/shaders/galaxy/fragment.glsl
@@ -7,7 +7,11 @@ void main() {
 
     // Diffuse Point Pattern
     // Multiply by 2 as the gradient gets upto one and we were cutting only upto 0.5;
-    float strength =  1.0 - 2.0 * distance(gl_PointCoord, vec2(0.5));
+    // float strength =  1.0 - 2.0 * distance(gl_PointCoord, vec2(0.5));
+
+    // Light Point Pattern
+    float strength =  (1.0 - distance(gl_PointCoord, vec2(0.5)));
+    strength = pow(strength, 10.0);
 
     gl_FragColor = vec4(vec2(strength * gl_PointCoord), strength, 1.0);
 }
