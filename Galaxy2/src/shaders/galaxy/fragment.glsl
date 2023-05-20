ce003fe424f3564f37f8c3b9ab6b907353821ac8
Akash Shaw <shawakash2003@gmail.com>
2023-05-20 14:02:51 +0530

    Gradient to circle

--- Galaxy2/src/shaders/galaxy/fragment.glsl
+++ Galaxy2/src/shaders/galaxy/fragment.glsl
@@ -3,5 +3,5 @@
 void main() {
     float strength = step(0.5, 1.0 - distance(gl_PointCoord, vec2(0.5)));
 
-    gl_FragColor = vec4(vec3(strength), 1.0);
+    gl_FragColor = vec4(vec2(strength * gl_PointCoord), strength, 1.0);
 }
