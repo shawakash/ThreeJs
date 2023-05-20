755cf8a85e7170b0fa9d076fbc50b1b78733e225
Akash Shaw <shawakash2003@gmail.com>
2023-05-20 13:35:05 +0530

    Sending Pixel Ratio

--- Galaxy2/src/script.js
+++ Galaxy2/src/script.js
@@ -97,7 +97,7 @@ const generateGalaxy = () => {
         fragmentShader,
 
         uniforms: {
-            uSize: { value: 0.8 },
+            uSize: { value: 0.8 * renderer.getPixelRatio() },
         }
 
     })
@@ -113,7 +113,6 @@ const generateGalaxy = () => {
     scene.add(points)
 }
 
-generateGalaxy()
 
 gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
 gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
@@ -125,7 +124,7 @@ gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
 
 /**
  * Sizes
- */
+*/
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -135,11 +134,11 @@ window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
-
+    
     // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
-
+    
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -147,7 +146,7 @@ window.addEventListener('resize', () => {
 
 /**
  * Camera
- */
+*/
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 3
@@ -170,20 +169,21 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
- */
+*/
 const clock = new THREE.Clock()
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
-
+    
     // Update controls
     controls.update()
-
+    
     // Render
     renderer.render(scene, camera)
-
+    
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
+generateGalaxy()
 tick()
