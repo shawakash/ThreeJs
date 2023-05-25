641c9ac4858e7361768522c4507af4f2f76e26ce
Akash Shaw <shawakash2003@gmail.com>
2023-05-25 19:47:29 +0530

    Using useMemo to save positions array

--- fibre/src/components/CustomObject.jsx
+++ fibre/src/components/CustomObject.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import { Float16BufferAttribute } from 'three';
+import React, { useMemo } from 'react'
+import { DoubleSide, Float16BufferAttribute } from 'three';
 
 const CustomObject = () => {
 
+
+    
     const vertexCount = 10 * 3;
-    const positions = new Float32Array(vertexCount * 3);
+    
+    const positions = useMemo(() => {
+        const positions = new Float32Array(vertexCount * 3);
+        
+        for(let i=0; i<vertexCount * 3; i++) {
+            positions[i] = (Math.random() - 0.5) * 3;
+        }
 
-    for(let i=0; i<vertexCount * 3; i++) {
-        positions[i] = (Math.random() - 0.5) * 3;
-    }
+        return positions;
 
+    }, [])
 
     // <bufferAttribute /> is a alertnative for geometry.attributes and attach is attaching to positions attribute
 
@@ -24,7 +31,7 @@ const CustomObject = () => {
                         array={ positions }
                     />
                 </bufferGeometry>
-                <meshBasicMaterial />
+                <meshBasicMaterial side={DoubleSide}/>
             </mesh>
         </>
     )
