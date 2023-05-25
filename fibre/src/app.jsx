11164cb8ec75d6379f715ebff32664e8a828f744
Akash Shaw <shawakash2003@gmail.com>
2023-05-25 17:25:19 +0530

    Resizing Canvas

--- fibre/src/app.jsx
+++ fibre/src/app.jsx
@@ -7,13 +7,6 @@ const App = () => {
 
     return (
         <>
-            {/* <mesh position={[ 2, 2, 2 ]} rotation-x={ 0.5 }>
-            <boxGeometry />
-            <boxMeshBasicMaterial color='red' />
-        </mesh> */}
-            {/* <Canvas></Canvas> */}
-            <div>app</div>
-
             <Canvas>
                 <mesh>
                     <torusKnotGeometry />
