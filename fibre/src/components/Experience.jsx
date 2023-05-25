164b85c52476c192a05d45c9fb86f74d6b6e81d6
Akash Shaw <shawakash2003@gmail.com>
2023-05-25 18:14:48 +0530

    Task

--- fibre/src/components/Experience.jsx
+++ fibre/src/components/Experience.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DoubleSide } from 'three'
 
 
 // Args are used to send initial arguments to geometry
@@ -10,21 +11,38 @@ import React from 'react'
 const Experience = () => {
     return (
         <>
-            <group position={[-4, 0, 0]} >
-                    <mesh rotation-x={ Math.PI * -0.25 } rotation-y={ Math.PI * 0.25 }>
+            <group position={[0, 0, 0]} >
+                    {/* <mesh rotation-x={ Math.PI * -0.25 } rotation-y={ Math.PI * 0.25 }>
                         <torusGeometry />
                         <meshNormalMaterial />
-                    </mesh>
+                    </mesh> */}
                     {/* <mesh position={[ 4, 0, 0 ]}  scale={[ 0.5, 0.5, 0.5 ]}> */}
-                    <mesh position-x={ 4 }  scale={ 0.5 }>
-                        <sphereGeometry args={[ 2, 60, 60 ]}/>
+                    {/* <mesh position-x={ 4 }  scale={ 0.5 }> */}
+                        {/* <sphereGeometry args={[ 2, 60, 60 ]}/> */}
                         {/* <meshBasicMaterial args={[{ color: 'red', wireframe: true }]}/>   */ } 
-                        <meshBasicMaterial color='cyan' wireframe={ true }/>
-                    </mesh>
-                    <mesh position={[8, 0, 0]} rotation-y={0.1}>
+                        {/* <meshBasicMaterial color='cyan' wireframe={ true }/> */}
+                    {/* </mesh> */}
+                    {/* <mesh position={[8, 0, 0]} rotation-y={0.1}>
                         <torusKnotGeometry />
                         <meshNormalMaterial />
+                    </mesh> */}
+
+
+                    <mesh position-x={-4}>
+                        <sphereGeometry />
+                        <meshBasicMaterial color={'#ff7321'} wireframe/>
                     </mesh>
+
+                    <mesh position-x={4}>
+                        <boxGeometry />
+                        <meshBasicMaterial color={'mediumpurple'}/>
+                    </mesh>
+
+                    <mesh rotation-x={ -Math.PI * 0.45 } position-y={-10}>
+                        <planeGeometry args={[ 100, 100, 5, 50, 50, 50 ]}/>
+                        <meshBasicMaterial color={'green'} side={DoubleSide}/>
+                    </mesh>
+
                 </group>
         </>
     )
