00af46a75584f3a57a0ee09ff07d620f63e9f12d
Akash Shaw <shawakash2003@gmail.com>
2023-05-25 19:57:36 +0530

    using useEffect to calculate vertex normal

--- fibre/src/components/CustomObject.jsx
+++ fibre/src/components/CustomObject.jsx
@@ -1,9 +1,14 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 import { DoubleSide, Float16BufferAttribute } from 'three';
 
 const CustomObject = () => {
 
+    const geometryRef = useRef();
 
+    // if(geometryRef.current) {
+    //     // computeVertexNormal - computes the normal
+    //     geometryRef.current.computeVertexNormals();
+    // }
     
     const vertexCount = 10 * 3;
     
@@ -19,11 +24,17 @@ const CustomObject = () => {
     }, [])
 
     // <bufferAttribute /> is a alertnative for geometry.attributes and attach is attaching to positions attribute
+    // Initial the mesh won't respond to the light as normals of the geometries are missing and we can do math and create a new normal attribute
+    // but we will tell three js to do that
+
+    useEffect(() => {
+        geometryRef.current.computeVertexNormals();
+    }, [])
 
     return (
         <> 
             <mesh>
-                <bufferGeometry>
+                <bufferGeometry ref={geometryRef}>
                     <bufferAttribute 
                         attach={'attributes-position'}
                         count={vertexCount}
@@ -31,7 +42,7 @@ const CustomObject = () => {
                         array={ positions }
                     />
                 </bufferGeometry>
-                <meshBasicMaterial side={DoubleSide}/>
+                <meshStandardMaterial side={DoubleSide} color={'mediumpurple'}/>
             </mesh>
         </>
     )
