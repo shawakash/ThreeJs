c82ff2d05fe7e4ccbf0e661c0ae68b1b33af927a
Akash Shaw <shawakash2003@gmail.com>
2023-05-25 18:32:28 +0530

    Animation, tick in R3F, useFrame, useRef, Clock

--- fibre/src/components/Experience.jsx
+++ fibre/src/components/Experience.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import { DoubleSide } from 'three'
+import { useFrame } from '@react-three/fiber'
+import React, { useRef } from 'react'
+import { Clock, DoubleSide } from 'three'
 
 
 // Args are used to send initial arguments to geometry
 // We provide array in props as some propeties in three js required a vector3
 // Target a specific axis by appending -x, -y, -z to the property
 // Don't provide a string to a number type prop, a bug
+// Useframe is used as tick function in three js provided by the  react three fiber
+
+/**
+ * 
+ * DON'T USE USESTATE TO ANIMATE THING AS IT RERENDERS AND ALL DATA GETS LOST
+ * 
+ */
 
 
 const Experience = () => {
+
+    const cubeRef = useRef();
+
+    const clock = new Clock;
+    // clock.start();
+
+    const tick = useFrame(() => {
+        
+        const elapsedTime = clock.getElapsedTime();
+
+        if(cubeRef.current) {
+            
+            cubeRef.current.rotation.y = elapsedTime * 0.75;
+        }
+
+    });
+
+    // tick;    // No need to call the useFrame it is already called by default
+
     return (
         <>
             <group position={[0, 0, 0]} >
@@ -33,12 +60,12 @@ const Experience = () => {
                         <meshBasicMaterial color={'#ff7321'} wireframe/>
                     </mesh>
 
-                    <mesh position-x={4}>
+                    <mesh ref={cubeRef} position-x={4}>
                         <boxGeometry />
                         <meshBasicMaterial color={'mediumpurple'}/>
                     </mesh>
 
-                    <mesh rotation-x={ -Math.PI * 0.45 } position-y={-10}>
+                    <mesh rotation-x={ -Math.PI * 0.5 } position-y={-10}>
                         <planeGeometry args={[ 100, 100, 5, 50, 50, 50 ]}/>
                         <meshBasicMaterial color={'green'} side={DoubleSide}/>
                     </mesh>
