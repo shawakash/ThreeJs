384a02bf937cfb4a5a2c64156d25dfe769deace6
Akash Shaw <shawakash2003@gmail.com>
2023-05-26 16:17:14 +0530

    Different ways of changing background color

--- env/src/index.jsx
+++ env/src/index.jsx
@@ -1,9 +1,18 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
+import Experience from './components/Experience'
+import { Color } from 'three'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+const created = ({ gl, scene }) => {
+
+    /*1*/   // gl.setClearColor('black', 1)
+    /*2*/  // scene.background = new Color('black')
+    /*3*/ // css
+
+}
 
 root.render(
     <Canvas
@@ -13,6 +22,8 @@ root.render(
             far: 200,
             position: [ - 4, 3, 6 ]
         } }
+
+        onCreated={ created }
     >
         <Experience />
     </Canvas>
