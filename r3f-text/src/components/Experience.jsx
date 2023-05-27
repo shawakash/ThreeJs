8f2318f0504c5015fe20b34d3ad3c4014808b925
Akash Shaw <shawakash2003@gmail.com>
2023-05-27 14:27:50 +0530

    Different way to add a common material and geometry

--- r3f-text/src/components/Experience.jsx
+++ r3f-text/src/components/Experience.jsx
@@ -1,6 +1,10 @@
 import { Center, Environment, OrbitControls, Text3D, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import * as THREE from 'three'
+
+const dognutGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
+const torusMaterial = new THREE.MeshMatcapMaterial();
 
 export default function Experience() {
 
@@ -9,8 +13,13 @@ export default function Experience() {
     const [textMatcapTexture] = useMatcapTexture('3E2335_D36A1B_8E4A2E_2842A5', 256);
     const [dognutMatcapTexture] = useMatcapTexture('5E5855_C6C4CD_C89B67_8F8E98', 256);
 
-    const [torusGeometry, setTorusGeometry] = useState();
-    const [torusMaterial, setTorusMaterial] = useState();
+    // const [torusGeometry, setTorusGeometry] = useState();
+    // const [torusMaterial, setTorusMaterial] = useState();
+
+    useEffect(() => {
+        torusMaterial.matcap = dognutMatcapTexture;
+        torusMaterial.needsUpdate = true;
+    }, [])
 
     const tempArray = [...Array(100)];
 
@@ -20,8 +29,8 @@ export default function Experience() {
 
         <OrbitControls makeDefault />
 
-        <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
-        <meshMatcapMaterial ref={setTorusMaterial} matcap={dognutMatcapTexture} />
+        {/* <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
+        <meshMatcapMaterial ref={setTorusMaterial} matcap={dognutMatcapTexture} /> */}
 
         <Center>
             <Text3D
@@ -39,29 +48,29 @@ export default function Experience() {
                 <meshMatcapMaterial matcap={textMatcapTexture} />
             </Text3D>
 
-
-            {[...Array(200)].map((value, index) =>
-                <mesh
-                    key={index}
-                    geometry={torusGeometry}
-                    material={torusMaterial}
-                    position={[
-                        (Math.random() - 0.5) * 100,
-                        (Math.random() - 0.5) * 100,
-                        (Math.random() - 0.5) * 100
-                    ]}
-                    rotation={[
-                        (Math.random() - 0.5) * Math.PI,
-                        (Math.random() - 0.5) * Math.PI,
-                        (Math.random() - 0.5) * Math.PI
-                    ]}
-                    scale={Math.random() * 0.6 + 0.3}
-                />
-            )}
-
-
-
         </Center>
+
+        {[...Array(200)].map((value, index) =>
+            <mesh
+                key={index}
+                geometry={dognutGeometry}
+                material={torusMaterial}
+                position={[
+                    (Math.random() - 0.5) * 100,
+                    (Math.random() - 0.5) * 100,
+                    (Math.random() - 0.5) * 100
+                ]}
+                rotation={[
+                    (Math.random() - 0.5) * Math.PI,
+                    (Math.random() - 0.5) * Math.PI,
+                    (Math.random() - 0.5) * Math.PI
+                ]}
+                scale={Math.random() * 0.6 + 0.3}
+            />
+        )}
+
+
+
 
 
 
