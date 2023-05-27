9f06fdf5ccae09dd0c511503d7cc6bb07a2dc501
Akash Shaw <shawakash2003@gmail.com>
2023-05-27 14:14:27 +0530

    Adding a single geometry and material to all dognuts

--- r3f-text/src/components/Experience.jsx
+++ r3f-text/src/components/Experience.jsx
@@ -1,7 +1,6 @@
 import { Center, Environment, OrbitControls, Text3D, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { Suspense } from 'react';
-import Donuts from './Donuts';
+import { useState } from 'react';
 
 export default function Experience() {
 
@@ -10,6 +9,9 @@ export default function Experience() {
     const [textMatcapTexture] = useMatcapTexture('3E2335_D36A1B_8E4A2E_2842A5', 256);
     const [dognutMatcapTexture] = useMatcapTexture('5E5855_C6C4CD_C89B67_8F8E98', 256);
 
+    const [torusGeometry, setTorusGeometry] = useState();
+    const [torusMaterial, setTorusMaterial] = useState();
+
     const tempArray = [...Array(100)];
 
     return <>
@@ -17,6 +19,9 @@ export default function Experience() {
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
+
+        <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
+        <meshMatcapMaterial ref={setTorusMaterial} matcap={dognutMatcapTexture} />
 
         <Center>
             <Text3D
@@ -35,8 +40,11 @@ export default function Experience() {
             </Text3D>
 
 
-            {[...Array(200)].map(() =>
+            {[...Array(200)].map((value, index) =>
                 <mesh
+                    key={index}
+                    geometry={torusGeometry}
+                    material={torusMaterial}
                     position={[
                         (Math.random() - 0.5) * 100,
                         (Math.random() - 0.5) * 100,
@@ -48,10 +56,7 @@ export default function Experience() {
                         (Math.random() - 0.5) * Math.PI
                     ]}
                     scale={Math.random() * 0.6 + 0.3}
-                >
-                    <torusGeometry args={[1, 0.6, 16, 32]} />
-                    <meshMatcapMaterial matcap={dognutMatcapTexture} />
-                </mesh>
+                />
             )}
 
 
