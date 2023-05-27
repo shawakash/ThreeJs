7550c14ec88d8735eeb5bf2d8c521b4e357b5e8b
Akash Shaw <shawakash2003@gmail.com>
2023-05-27 14:01:12 +0530

    Adding Random dognut

--- r3f-text/src/components/Experience.jsx
+++ r3f-text/src/components/Experience.jsx
@@ -1,13 +1,14 @@
 import { Center, Environment, OrbitControls, Text3D, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { Suspense } from 'react';
+import Donuts from './Donuts';
 
 export default function Experience() {
 
     // const matcapTexture = useMatcapTexture('3E2335_D36A1B_8E4A2E_2842A5', 256).find(value => value.isTexture);
     // const matcapTexture = useMatcapTexture('3E2335_D36A1B_8E4A2E_2842A5', 256)[0];
-    const [ matcapTexture ] = useMatcapTexture('3E2335_D36A1B_8E4A2E_2842A5', 256);
-    console.log(matcapTexture)
+    const [textMatcapTexture] = useMatcapTexture('3E2335_D36A1B_8E4A2E_2842A5', 256);
+    const [dognutMatcapTexture] = useMatcapTexture('5E5855_C6C4CD_C89B67_8F8E98', 256);
 
     const tempArray = [...Array(100)];
 
@@ -30,14 +31,30 @@ export default function Experience() {
                 bevelSegments={5}
             >
                 Hola
-                <meshMatcapMaterial matcap={ matcapTexture } />
+                <meshMatcapMaterial matcap={textMatcapTexture} />
             </Text3D>
 
-            <Suspense
-                // fallback={}
-            >
-                
-            </Suspense>
+
+            {[...Array(200)].map(() =>
+                <mesh
+                    position={[
+                        (Math.random() - 0.5) * 100,
+                        (Math.random() - 0.5) * 100,
+                        (Math.random() - 0.5) * 100
+                    ]}
+                    rotation={[
+                        (Math.random() - 0.5) * Math.PI,
+                        (Math.random() - 0.5) * Math.PI,
+                        (Math.random() - 0.5) * Math.PI
+                    ]}
+                    scale={Math.random() * 0.6 + 0.3}
+                >
+                    <torusGeometry args={[1, 0.6, 16, 32]} />
+                    <meshMatcapMaterial matcap={dognutMatcapTexture} />
+                </mesh>
+            )}
+
+
 
         </Center>
 
