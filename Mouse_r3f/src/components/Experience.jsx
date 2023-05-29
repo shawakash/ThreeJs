db20d7b87eca9d5a08c419f48c7c95d8007311dc
Akash Shaw <shawakash2003@gmail.com>
2023-05-29 16:05:13 +0530

    Different Events

--- Mouse_r3f/src/components/Experience.jsx
+++ Mouse_r3f/src/components/Experience.jsx
@@ -4,12 +4,10 @@ import { Perf } from 'r3f-perf'
 import { useRef } from 'react'
 import { Color } from 'three'
 
-export default function Experience()
-{
+export default function Experience() {
     const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
+
+    useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2
     })
 
@@ -24,20 +22,33 @@ export default function Experience()
         <Perf position={'top-left'} />
         <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+        <directionalLight position={[1, 2, 3]} intensity={1.5} />
+        <ambientLight intensity={0.5} />
 
-        <mesh position-x={ - 2 }>
+        <mesh position-x={- 2}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 } onContextMenu={eventHandler}>
+        <mesh
+            ref={cube}
+            position-x={2}
+            scale={1.5}
+            onClick={eventHandler}
+            // onContextMenu={eventHandler}        // Right Click
+            // onDoubleClick={eventHandler}
+            // onPointerUp={eventHandler}    // When We release The click (left or right)
+            // onPointerDown={eventHandler}    // When We clicked (left or right)
+            // onPointerOver={eventHandler}    // When the cursor or finger just went above or enter or touched the object
+            // onPointerEnter={eventHandler}    // When the cursor or finger just went above or enter or touched the object
+            // onPointerMove={eventHandler}    // When the cursor or finger moves over the object
+            // onPointerMissed={eventHandler}    // When the cursor or finger misses over the object
+        >
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
