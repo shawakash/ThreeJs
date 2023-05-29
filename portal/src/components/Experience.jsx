c466c06e50252357e046a404930220d5e596a2c8
Akash Shaw <shawakash2003@gmail.com>
2023-05-29 13:09:51 +0530

    Adding Baked Model

--- portal/src/components/Experience.jsx
+++ portal/src/components/Experience.jsx
@@ -1,15 +1,27 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF } from '@react-three/drei'
 
 export default function Experience()
 {
+
+    const { nodes } = useGLTF('./model/portal.glb', true);
+    console.log(nodes)
+
+    // const { scene }
+
     return <>
 
         <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
+        {/* Test Cube */}
+        {/* <mesh scale={ 1.5 }>
             <boxGeometry />
             <meshNormalMaterial />
-        </mesh>
+        </mesh> */}
+
+        <mesh 
+            geometry={nodes.baked.geometry}
+            material={nodes.baked.material}
+        />
 
     </>
 }
