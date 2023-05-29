959fb039b0af14de50b47703fe5695ffafd4c44d
Akash Shaw <shawakash2003@gmail.com>
2023-05-29 16:48:00 +0530

    A bug, we need to separate the click of complex models as if stacked on clicking in the intersection area all other mesh get triggered

--- Mouse_r3f/src/components/Hamburger.jsx
+++ Mouse_r3f/src/components/Hamburger.jsx
@@ -5,8 +5,20 @@ const Hamburger = () => {
 
     const model = useGLTF('./hamburger.glb');
 
+    const eventHandler = (event) => {
+        model.scene.scale.set(0.35, 0.35, 0.35)
+        console.log('CLick')
+    }
+
     return (
-        <primitive object={model.scene} scale={ 0.35 } />
+        <primitive
+            object={model.scene}
+            scale={0.25}
+            position-y={ 0.5 }
+            onPointerEnter={() => {document.body.style.cursor = 'pointer'}}
+            onPointerLeave={() => {document.body.style.cursor = 'default'}}
+            onClick={eventHandler}
+        />
     )
 }
 
