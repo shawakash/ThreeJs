0e5cf436e8fe8ced246fb5d31e10bdf004ee6cf4
Akash Shaw <shawakash2003@gmail.com>
2023-05-29 17:42:45 +0530

    Vignette a effect that add darkness in a circular pattern high on the corners

--- post_r3f/src/components/Experience.jsx
+++ post_r3f/src/components/Experience.jsx
@@ -1,34 +1,38 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { EffectComposer } from '@react-three/postprocessing'
+import { EffectComposer, Vignette } from '@react-three/postprocessing'
 
-export default function Experience()
-{
+export default function Experience() {
     return <>
 
-        <EffectComposer>
-            
+        <EffectComposer
+        // multisampling={0}
+        >
+            <Vignette 
+                offset={0.3}
+                darkness={0.7}
+            /> 
         </EffectComposer>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+        <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+        <ambientLight intensity={0.5} />
 
 
-        <mesh castShadow position-x={ - 2 }>
+        <mesh castShadow position-x={- 2}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
+        <mesh castShadow position-x={2} scale={1.5}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
