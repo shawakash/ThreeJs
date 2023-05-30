cacb97e5035285cabd5c016cb976f16862bb2b35
Akash Shaw <shawakash2003@gmail.com>
2023-05-30 17:18:07 +0530

    Adding Model

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -1,15 +1,22 @@
-import { OrbitControls } from '@react-three/drei'
+import { Center, OrbitControls, useGLTF } from '@react-three/drei'
 
-export default function Experience()
-{
+export default function Experience() {
+
+    const macBook = useGLTF(`https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf`, true);
+
+
     return <>
 
         <OrbitControls makeDefault />
-        
+
         <mesh>
             <boxGeometry />
             <meshNormalMaterial />
         </mesh>
+
+        <Center>
+            <primitive object={macBook.scene} />
+        </Center>
 
     </>
 }
