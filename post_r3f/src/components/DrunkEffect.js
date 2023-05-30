d6aa1698151d0c204d27a0e1f9fe383ec7fe2fed
Akash Shaw <shawakash2003@gmail.com>
2023-05-30 13:34:31 +0530

    Creating a Custom effect with effect postprocessing

---
+++ post_r3f/src/components/DrunkEffect.js
@@ -0,0 +1,15 @@
+import  { Effect } from 'postprocessing'
+
+const fragmentShaders = /* glsl */`
+    void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outpuColor) {
+
+    }
+`
+
+export default class DrunkEffect extends Effect {
+
+    constructor() {
+
+    }
+
+}
