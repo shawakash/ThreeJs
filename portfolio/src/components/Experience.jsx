ef666d4defc3d6f388b3c8b1b63cba8a245e6cbb
Akash Shaw <shawakash2003@gmail.com>
2023-05-30 17:36:28 +0530

    Presentation Control

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { Center, Environment, Float, OrbitControls, useGLTF } from '@react-three/drei'
+import { Center, Environment, Float, PresentationControls, useGLTF } from '@react-three/drei'
 
 export default function Experience() {
 
@@ -13,14 +13,22 @@ export default function Experience() {
 
         </Environment>
 
-        <OrbitControls makeDefault />
 
-        <Float rotationIntensity={0.4}>
-            <primitive
-                object={macBook.scene}
-                position-y={-1.2}
-            />
-        </Float>
+        <PresentationControls
+            global
+            rotation={[0.13, 0.1, 0]}
+            polar={[-0.4, 0.2]}
+            azimuth={[-1, 0.75]}
+            config={{ mass: 2, tension: 400 }}
+            snap={{ mass: 4, tension: 400 }}
+        >
+            <Float rotationIntensity={0.4}>
+                <primitive
+                    object={macBook.scene}
+                    position-y={-1.2}
+                />
+            </Float>
+        </PresentationControls>
 
     </>
 }
