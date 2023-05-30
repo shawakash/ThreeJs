edefd3d03ac82c8493afdfee7f9cf4d7a98c96d5
Akash Shaw <shawakash2003@gmail.com>
2023-05-30 13:01:23 +0530

    Adding Props to understand attributes

--- post_r3f/src/components/Experience.jsx
+++ post_r3f/src/components/Experience.jsx
@@ -2,8 +2,43 @@ import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { Bloom, DepthOfField, EffectComposer, Glitch, Noise, SSR, Vignette } from '@react-three/postprocessing'
 import { GlitchMode, BlendFunction } from 'postprocessing'
+import { useControls } from 'leva'
 
 export default function Experience() {
+
+
+    const ssr = useControls({
+        temporalResolve: true,
+        STRETCH_MISSED_RAYS: true,
+        USE_MRT: true,
+        USE_NORMALMAP: true,
+        USE_ROUGHNESSMAP: true,
+        ENABLE_JITTERING: true,
+        ENABLE_BLUR: true,
+        temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+        temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+        maxSamples: { value: 0, min: 0, max: 1 },
+        resolutionScale: { value: 1, min: 0, max: 1 },
+        blurMix: { value: 0.5, min: 0, max: 1 },
+        blurKernelSize: { value: 8, min: 0, max: 8 },
+        blurSharpness: { value: 0.5, min: 0, max: 1 },
+        rayStep: { value: 0.3, min: 0, max: 1 },
+        intensity: { value: 1, min: 0, max: 5 },
+        maxRoughness: { value: 0.1, min: 0, max: 1 },
+        jitter: { value: 0.7, min: 0, max: 5 },
+        jitterSpread: { value: 0.45, min: 0, max: 1 },
+        jitterRough: { value: 0.1, min: 0, max: 1 },
+        roughnessFadeOut: { value: 1, min: 0, max: 1 },
+        rayFadeOut: { value: 0, min: 0, max: 1 },
+        MAX_STEPS: { value: 20, min: 0, max: 20 },
+        NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+        maxDepthDifference: { value: 3, min: 0, max: 10 },
+        maxDepth: { value: 1, min: 0, max: 1 },
+        thickness: { value: 10, min: 0, max: 10 },
+        ior: { value: 1.45, min: 0, max: 2 }
+    })
+
+
     return <>
 
         {/* In r3f post processing occurs in blending i.e. effects are blended using some principle */}
@@ -39,7 +74,7 @@ export default function Experience() {
                 bokehScale={6}
             /> */}
 
-            <SSR />
+            <SSR {...ssr} />
         </EffectComposer>
 
         <Perf position="top-left" />
@@ -64,7 +99,7 @@ export default function Experience() {
 
         <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
+            <meshStandardMaterial color="black" metalness={0} roughness={0} />
         </mesh>
 
     </>
--- post_r3f/src/index.jsx
+++ post_r3f/src/index.jsx
@@ -2,6 +2,7 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './components/Experience.jsx'
+import { Leva } from 'leva'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
