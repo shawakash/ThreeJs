198aa87cf56b0612594ead13abc2c45d28145bf8
Akash Shaw <shawakash2003@gmail.com>
2023-05-31 15:04:00 +0530

    Object rotation and translation

--- physics_r3f/src/components/Experience.jsx
+++ physics_r3f/src/components/Experience.jsx
@@ -2,11 +2,24 @@ import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { BallCollider, CapsuleCollider, ConeCollider, CuboidCollider, Physics, RigidBody, RoundCuboidCollider, TrimeshCollider } from '@react-three/rapier'
 import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Euler, Quaternion } from 'three';
 
 export default function Experience() {
 
     const cube = useRef();
-    const cubeMesh = useRef();
+
+    const twister = useRef();
+
+    useFrame((state, delta) => {
+        const elapsedTime = state.clock.getElapsedTime();
+        const euler = new Euler(0, elapsedTime, 0);
+        const quaterion = new Quaternion();
+        quaterion.setFromEuler(euler);
+
+        twister.current.setNextKinematicRotation(quaterion);
+        twister.current.setNextKinematicTranslation({ x: Math.cos(elapsedTime * Math.PI), y: -0.8, z: Math.sin(elapsedTime * Math.PI) })
+    })
 
     return <>
 
@@ -25,17 +38,17 @@ export default function Experience() {
                 </mesh>
             </RigidBody>
 
-            {/* <RigidBody colliders={'trimesh'} rotation={[Math.PI * 0.5, 0, 0]} position-y={1}>
-                {/* <CuboidCollider args={[1.5, 1.5, 0.5]} />
+            {/* <RigidBody colliders={'trimesh'} rotation={[Math.PI * 0.5, 0, 0]} position-y={1}> */}
+            {/* <CuboidCollider args={[1.5, 1.5, 0.5]} />
                 <CuboidCollider
                     args={[0.25, 1, 0.25]}
                     position={[0, 0, 1]}
                     rotation={[-Math.PI * 0.35, 0, 0]}
-                /> */}
-            {/* <BallCollider args={[1.5]} /> */}
-            {/* <RoundCuboidCollider args={[1,1,1,1]} /> */}
-            {/* <CapsuleCollider args={[1,1]} /> */}
-            {/* <ConeCollider args={[1,1,1]} /> */}
+                />
+                <BallCollider args={[1.5]} />
+                <RoundCuboidCollider args={[1, 1, 1, 1]} />
+                <CapsuleCollider args={[1, 1]} />
+                <ConeCollider args={[1, 1, 1]} /> */}
             {/* <mesh castShadow>
                     <torusGeometry args={[1, 0.5, 16, 32]} />
                     <meshStandardMaterial color={'mediumpurple'} />
@@ -48,7 +61,8 @@ export default function Experience() {
                 restitution={0.7}                   // apply the same value of 1 to the floor to have ideal effect
                 friction={0.7}
                 colliders={false}           // Needed for mass
-                position={[1.5, 2, 0]}
+                position={[4, 0, 0]}
+                type='dynamic'
             >
                 {/* For mass Custom Coliders are needed */}
                 <CuboidCollider args={[0.5, 0.5, 0.5]} mass={1} />
@@ -57,8 +71,8 @@ export default function Experience() {
                     onClick={(e) => {
                         const mass = cube.current.mass();
 
-                        cube.current.applyImpulse({x: 0, y: 7 * mass, z: 0})
-                        // cube.current.applyTorqueImpulse({ x: 0, y: 2 * mass, z: 2 * mass })
+                        // cube.current.applyImpulse({x: 0, y: 7 * mass, z: 0})
+                        cube.current.applyTorqueImpulse({ x: 0, y: 2 * mass, z: 2 * mass })
                         // cube.current.applyImpulseAtPoint({ x: 0, y: 2, z: 0 }, { x: 1, y: 1.5, z: 0.5 })
 
                     }}
@@ -67,6 +81,24 @@ export default function Experience() {
                     <meshStandardMaterial color="mediumpurple" />
                 </mesh>
             </RigidBody>
+
+
+            <RigidBody
+                ref={twister}
+                position={[0, -0.8, 0]}
+                friction={0}
+                type='kinematicPosition'
+            >
+                <mesh
+                    scale={[0.4, 0.4, 3]}
+                    castShadow
+                >
+                    <boxGeometry />
+                    <meshStandardMaterial color="red" />
+                </mesh>
+            </RigidBody>
+
+
             <RigidBody
                 type='fixed'
                 // restitution={0.7}
