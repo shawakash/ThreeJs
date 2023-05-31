846354c07341c0e137d675f087f39ed6df711774
Akash Shaw <shawakash2003@gmail.com>
2023-05-31 13:35:26 +0530

    ApplyImpulse on the rigidbody

--- physics_r3f/src/components/Experience.jsx
+++ physics_r3f/src/components/Experience.jsx
@@ -1,8 +1,12 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { BallCollider, CapsuleCollider, ConeCollider, CuboidCollider, Physics, RigidBody, RoundCuboidCollider, TrimeshCollider } from '@react-three/rapier'
+import { useRef } from 'react';
 
 export default function Experience() {
+
+    const cube = useRef();
+
     return <>
 
         <Perf position="top-left" />
@@ -20,29 +24,35 @@ export default function Experience() {
                 </mesh>
             </RigidBody>
 
-            <RigidBody colliders={'trimesh'} rotation={[Math.PI * 0.5, 0, 0]} position-y={1}>
+            {/* <RigidBody colliders={'trimesh'} rotation={[Math.PI * 0.5, 0, 0]} position-y={1}>
                 {/* <CuboidCollider args={[1.5, 1.5, 0.5]} />
                 <CuboidCollider
                     args={[0.25, 1, 0.25]}
                     position={[0, 0, 1]}
                     rotation={[-Math.PI * 0.35, 0, 0]}
                 /> */}
-                {/* <BallCollider args={[1.5]} /> */}
-                {/* <RoundCuboidCollider args={[1,1,1,1]} /> */}
-                {/* <CapsuleCollider args={[1,1]} /> */}
-                {/* <ConeCollider args={[1,1,1]} /> */}
-                <mesh castShadow>
+            {/* <BallCollider args={[1.5]} /> */}
+            {/* <RoundCuboidCollider args={[1,1,1,1]} /> */}
+            {/* <CapsuleCollider args={[1,1]} /> */}
+            {/* <ConeCollider args={[1,1,1]} /> */}
+            {/* <mesh castShadow>
                     <torusGeometry args={[1, 0.5, 16, 32]} />
                     <meshStandardMaterial color={'mediumpurple'} />
                 </mesh>
-            </RigidBody>
+            </RigidBody> */}
 
-            <RigidBody>
-                <mesh castShadow position={[3, 1, 3]}>
-                    <boxGeometry />
-                    <meshStandardMaterial color="mediumpurple" />
-                </mesh>
-                <mesh castShadow position={[3, 2, 1]} scale={[2, 1, 1]}>
+            <RigidBody ref={cube}>
+                <mesh
+                    castShadow
+                    position={[1.5, 2, 0]}
+                    onClick={(e) => {
+                        window.addEventListener('keypress', (ev) => {
+                            if(ev.code == 'Space') {
+                                cube.current.applyImpulse({x: 0, y: 7, z: 0})
+                            }
+                        })
+                    }}
+                >
                     <boxGeometry />
                     <meshStandardMaterial color="mediumpurple" />
                 </mesh>
